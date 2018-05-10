01586f71bd6f419ba3435c60266cb0083b8676d6
Matt Tilley <mtilley255@gmail.com>
2018-05-10 09:56:52 -0500

    Misc. changes per example.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,16 +6,21 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+        /*<button className="square" onClick={() => alert('click')}>*/
+        <button className = "square" onClick={alert('click')}>
+        {this.props.value}
       </button>
     );
   }
 }
 
+
+
+
+
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value = {i}/>;
   }
 
   render() {
@@ -43,6 +48,11 @@ class Board extends React.Component {
     );
   }
 }
+
+
+
+
+
 
 class Game extends React.Component {
   render() {
