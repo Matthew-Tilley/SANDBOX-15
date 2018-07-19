8f1d18ad2fa7ac3b922b2da02425d756807c5b7b
Matt Tilley <mtilley255@gmail.com>
2018-07-19 10:40:56 -0500

    Practice #2A

--- index.js
+++ index.js
@@ -6,6 +6,23 @@
 
 
 
+(2A)    More practice
+        So...this worked exactly as I expected.  The anonymous function
+        was assigned to the variable "funky."  When invoked, it returned
+        a string value of "HEY!" to the variable of "funky".  Thus, when
+        I call the function (wrapped in an alert) it outputs "HEY!" to
+        the screen.  Still no problem, conceptually...
+
+let funky = function(){
+    return "HEY!";
+}
+
+alert(funky());
+
+
+
+/*
+
 (2) Anonymous function method
     I intuitively recognize this as being an anonymous function; no 
     problem there.  I also realize it is invoked by calling the
@@ -18,6 +35,7 @@ let funky = function(){
 
 funky();
 
+*/
 
 
 
