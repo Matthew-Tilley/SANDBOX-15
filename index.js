93e0e48c50fdac8851371b510f74b88d78f2f488
Matt Tilley <mtilley255@gmail.com>
2018-07-19 10:31:37 -0500

    Practice #1

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/*  
+    YouTube tutorial
+    Code Explained / JavaScript Functions - PRACTICE
+*/
+
+// (1) Function declaration method - I totally understand this / no problem.
+
+function funky(){
+    
+    alert("HEY!");
+    
+}
+
+funky();
+
