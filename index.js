a62dbceb09b4e75463c6b651a64533678f1b605b
Matt Tilley <mtilley255@gmail.com>
2018-07-19 10:36:02 -0500

    Practice #2

--- index.js
+++ index.js
@@ -4,7 +4,27 @@
     Code Explained / JavaScript Functions - PRACTICE
 */
 
-// (1) Function declaration method - I totally understand this / no problem.
+
+
+(2) Anonymous function method
+    I intuitively recognize this as being an anonymous function; no 
+    problem there.  I also realize it is invoked by calling the
+    variable name it is assigned to, then adding parens to call it
+    as a function...no problem.
+
+let funky = function(){
+    alert("HEY!");
+}
+
+funky();
+
+
+
+
+
+/*
+
+(1) Function declaration method - I totally understand this / no problem.
 
 function funky(){
     
@@ -13,4 +33,6 @@ function funky(){
 }
 
 funky();
+
+*/
 
